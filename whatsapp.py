a35f91ee905662b7d58dcb39a74e0024464c0e91
abhishek <abhishekshah546@gmail.com>
2026-01-16 13:42:23 +0300

    manage attachments

--- whatsapp.py
+++ whatsapp.py
@@ -561,9 +561,9 @@ def send_manage_attachments_list(sender_id: str):
                     {
                         "title": "Attachments",
                         "rows": [
-                            {"id": LIST_ATTACH_PREVIEW, "title": "👀 Preview/List attachments"},
-                            {"id": LIST_ATTACH_REMOVE_LAST, "title": "🗑️ Remove last attachment"},
-                            {"id": LIST_ATTACH_CLEAR_ALL, "title": "🧹 Clear all attachments"},
+                            {"id": LIST_ATTACH_PREVIEW, "title": "Preview uploads"},
+                            {"id": LIST_ATTACH_REMOVE_LAST, "title": "Remove last"},
+                            {"id": LIST_ATTACH_CLEAR_ALL, "title": "🧹 Clear all"},
                         ],
                     }
                 ],
